b2dfca5e767d326784a4c6fbe290f94cebdd8f9b
Greg Gallardo <greggallardo@gmail.com>
2022-07-02 10:36:13 -0500

    adding custom timer to increase H-delay resolution

--- arduino/menacer/menacer.ino
+++ arduino/menacer/menacer.ino
@@ -4,16 +4,31 @@ const int VERTICAL_SYNC_1881 = 3;
 volatile int verticalLine = 0;
 const int TH_PIN = 8;
 
-int y = 102;  // line count
-int x = 45;      // delay
+int y = 140;  // line count
+int x = 200;  // Simulate TH delay
 
+
+// modified delayMicroseconds to use the smallest possible wait 
+// to increase horizontal resolution
+void delayX4Cycles(unsigned int c)
+{
+        if (--c == 0)
+                return;
+
+        // busy wait
+        __asm__ __volatile__ (
+                "1: sbiw %0,1" "\n\t" // 2 cycles
+                "brne 1b" : "=w" (c) : "0" (c) // 2 cycles
+        );
+}
 
 void compositeSyncInterrrupt() {
   verticalLine++;
   if( verticalLine == y ) {
-    delayMicroseconds( x );
+    //delayMicroseconds( x );
+    delayCycles(x);
     digitalWrite( TH_PIN, HIGH );
-    delayMicroseconds( 5 );
+    delayMicroseconds( 5 ); // arbitrary.  
     digitalWrite( TH_PIN, LOW );
   }
 }
@@ -35,7 +50,11 @@ void setup() {
 }
 
 void loop() {
+  while (Serial.available() > 0)
+{
+  x  = Serial.parseInt();
+ Serial.println(x);
+}
   // put your main code here, to run repeatedly:
-  Serial.println(verticalLine);
 
 }
