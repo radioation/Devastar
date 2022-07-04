661baf5607f49b49cf37d27d07fbdf73551f1008
Greg Gallardo <greggallardo@gmail.com>
2022-07-03 20:28:07 -0500

    working on blob processing

--- camera/camera.cpp
+++ camera/camera.cpp
@@ -20,7 +20,7 @@
 #define SERIAL_DEVICE "/dev/ttyACM0"
 
 #define SHOW_IMAGE
-#define SHOW_CALC
+//#define SHOW_CALC
 
 // intersect rectangle with a ray
 // * Ray is defined as R0 + t * D
@@ -74,8 +74,8 @@ int main()
 	// setup videocapture
 	cv::VideoCapture inputVideo;
 	inputVideo.open(0);
-	inputVideo.set(cv::CAP_PROP_FRAME_WIDTH, 1024);
-	inputVideo.set(cv::CAP_PROP_FRAME_HEIGHT, 720);
+	inputVideo.set(cv::CAP_PROP_FRAME_WIDTH, 640);
+	inputVideo.set(cv::CAP_PROP_FRAME_HEIGHT, 480);
 
 
 	// Setup object points
@@ -145,6 +145,7 @@ int main()
 	unsigned char xy[2];
 
 	// look at the cmameras
+	cv::Point2f pt;
 	while (true){
 		inputVideo >> frame;
 
@@ -168,11 +169,11 @@ int main()
 			int current = 0;
 			for( size_t i = 0; i < contours.size(); ++i ) {
 				// using size cutoff to find 'good' candidates.  Look at other features like circularity, position, etc.
-				if( moments[i].m00 > 50 && moments[i].m00 < 250 ) {
+				if( moments[i].m00 > 6 && moments[i].m00 < 250 ) {
 					centers[current] =  cv::Point2f( static_cast<float> ( moments[i].m10 / ( moments[i].m00 + 1e-5)), static_cast<float> ( moments[i].m01 / ( moments[i].m00 + 1e-5)) );
-#ifdef SHOW_CALC
+//#ifdef SHOW_CALC
 					std::cout << "center[" << current << "] = " << centers[current] <<  " area: " << moments[i].m00 <<std::endl;
-#endif
+//#endif
 					++current;
 					if( current == 4 ) {
 						break;
@@ -187,6 +188,7 @@ int main()
 				// draw point on image
 				cv::circle( displayCopy, center, 3.0f, cv::Scalar(0,0,255), 2.0f);
 			}
+			cv::circle( displayCopy, pt, 5.0f, cv::Scalar(0,255,255), 3.0f);
 
 			cv::imshow("points", displayCopy );
 			cv::waitKey(1);
@@ -196,7 +198,7 @@ int main()
 			// rvec- is the rotation vector
 			// tvec- is the translation vector 
 			cv::Mat rvec, tvec;
-			auto solveRet = cv::solvePnP(worldPoints, centers, cameraMatrix, distCoeffs, rvec, tvec, false, cv::SOLVEPNP_AP3P);
+			auto solveRet = cv::solvePnP(worldPoints, centers, cameraMatrix, distCoeffs, rvec, tvec, false); //, cv::SOLVEPNP_AP3P);
 
 			if( solveRet ) {
 				cv::Mat R;
@@ -212,9 +214,9 @@ int main()
 				R1[1] = tvec.at<double>(1);
 				R1[2] = tvec.at<double>(2);
 
-				D[0] =   R.at<double>(0, 2);
-				D[1] =   R.at<double>(1, 2);
-				D[2] =   R.at<double>(2, 2);
+				D[0] = R.at<double>(0, 2);
+				D[1] = R.at<double>(1, 2);
+				D[2] = R.at<double>(2, 2);
 
 				float u, v;
 				bool hit = intersectRect(R1, D, P0, S1, S2, width, height, u, v);
@@ -231,10 +233,17 @@ int main()
 				std::cout << "S1: " << S1 << std::endl;
 				std::cout << "S2: " << S2 << std::endl;
 
-				std::cout << "U: " << u << " V: " << v << std::endl;
 #endif
 
 				if( hit ) {
+#ifdef SHOW_IMAGE
+					std::cout << "U: " << u << " V: " << v << std::endl;
+					pt.x = (u/width) * 640.0f;
+					pt.y = (v/height) * 480.0f;
+					cv::circle( displayCopy, pt, 5.0f, cv::Scalar(0,255,255), 3.0f);
+					cv::imshow("points", displayCopy );
+					cv::waitKey(1);
+#endif
 					// from obvservation with delay4Cycles() on arduion
 					// X range is 73 to 269 : send 0 through 196
 					// Y range is 30 to 250 : send 0 through 220
@@ -253,6 +262,7 @@ int main()
 		auto ret = write( fd, offscreen, 2 );
 
 		}
+
 
 
 	}
