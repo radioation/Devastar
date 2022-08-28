9f64e64d9ac864256fb77ec87b1ee320235de362
Greg Gallardo <greggallardo@gmail.com>
2022-08-28 01:06:41 -0500

    Now using multiple scanlines to work with actual SMS games.

--- arduino/lm1881test/lm1881_phaser_hv_sync_test/lm1881_phaser_hv_sync_test.ino
+++ arduino/lm1881test/lm1881_phaser_hv_sync_test/lm1881_phaser_hv_sync_test.ino
@@ -29,10 +29,15 @@ const int PHASER_PIN_6 = 9;  // TL is the light gun trigger
 const int minY = 0;
 const int minX = 0;
 
-byte y = 136; // 24 appears to be the top of the screen, 247 the bottom  ( 247 -24  = 223  range )
-// My SMS Phaser appears to return 30 through 180 when I pan
+byte y = 124; //  SGDK: 24 appears to be the top of the screen, 247 the bottom  ( 247 -24  = 223  range )
+// Shooting gallary  ~32 is the top of the playfield, ~230 is the bottom ( 230 -32 = 198 range. but would need offset due to border)
+
+
+// My SGDK test with SMS Phaser appears to return 30 through 180 when I pan
 // across my TV screen in H32 mode.   so about 150 values over 320 pixels
-byte x = 193; // 15 appears to give me about 30, 193 gets me to about 180  ( 193-15 = 178 range )
+byte x = 100; // 15 appears to give me about 30, 193 gets me to about 180  ( 193-15 = 178 range )
+// Shooting gallary  ~22 is the left of the playfield, ~170 is the bottom ( 187 -22 = 165 range. but would need offset due to border)
+
 
 // modified delayMicroseconds to use the smallest possible wait
 // to increase horizontal resolution
@@ -48,13 +53,17 @@ void delayX4Cycles(unsigned int c)
   );
 }
 
+
 void compositeSyncInterrrupt() {
-  verticalLine++;
-  if ( verticalLine == minY + y ) { // && x != 255 ) {
+  verticalLine++; 
+  //if ( verticalLine == minY + y ) {  
+  // apparently we need to have more than one line
+  if ( verticalLine >= minY + y && verticalLine < minY + y + 7 ) {  
     delayX4Cycles(minX + x);
-    digitalWrite( PHASER_PIN_7, LOW );
+    digitalWrite( PHASER_PIN_7, LOW ); // TH active is 0
     delayMicroseconds( 5 ); // arbitrary.
-    digitalWrite( PHASER_PIN_7, HIGH );
+   digitalWrite( PHASER_PIN_7, HIGH );
+  //Serial.println(verticalLine);
   }
 }
 
@@ -73,8 +82,8 @@ void setup() {
   // controller pins 
   pinMode(PHASER_PIN_7, OUTPUT);
   pinMode(PHASER_PIN_6, OUTPUT); 
-  digitalWrite(PHASER_PIN_7, HIGH);
-  digitalWrite(PHASER_PIN_6, HIGH);
+  digitalWrite(PHASER_PIN_7, LOW);
+  digitalWrite(PHASER_PIN_6, LOW);
 
   // interrupts
   attachInterrupt(digitalPinToInterrupt(COMPOSITE_SYNC_1881), compositeSyncInterrrupt, RISING);
@@ -82,9 +91,10 @@ void setup() {
 }
 
 void loop() { 
+  Serial.println(verticalLine);
   // pull the trigger every once in awhile
-  delay(2000);
-  digitalWrite(PHASER_PIN_6, LOW); 
   delay(500);
   digitalWrite(PHASER_PIN_6, HIGH); 
+  delay(500);
+  digitalWrite(PHASER_PIN_6, LOW); 
 }
