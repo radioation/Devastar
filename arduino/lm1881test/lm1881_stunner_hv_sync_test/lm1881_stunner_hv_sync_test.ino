7dc88eda0c03439fac8e98e690a1768978fc40db
Greg Gallardo <radioation@users.noreply.github.com>
2022-09-11 11:55:50 -0500

    Adding keypress checks to set buttons and X/Y values

--- arduino/lm1881test/lm1881_stunner_hv_sync_test/lm1881_stunner_hv_sync_test.ino
+++ arduino/lm1881test/lm1881_stunner_hv_sync_test/lm1881_stunner_hv_sync_test.ino
@@ -18,25 +18,25 @@
 // sync variables
 const int COMPOSITE_SYNC_1881 = 2;
 const int VERTICAL_SYNC_1881 = 3;
-volatile int verticalLine = 0;
+volatile int verticalLine = 0;  // runnig count
 
 // controller pins
 const int STUNNER_PIN_4 = 8;  // TH and used to set the signal for the light gun
-const int STUNNER_PIN_6 = 9;  // TL is the light gun trigger
-const int STUNNER_PIN_5 = 10  // TR is the light gun start
+const int STUNNER_PIN_6_TRIGGER = 9;  // TL is the light gun trigger
+const int STUNNER_PIN_5_START = 10;  // TR is the light gun start
 
   
 // offsets
-const int minY = 0;
-const int minX = 0;
+volatile int minY = 0;
+volatile int minX = 0; 
 
-byte y = 124; //  SGDK: 24 appears to be the top of the screen, 247 the bottom  ( 247 -24  = 223  range )
+volatile byte y = 124; //  SGDK: 24 appears to be the top of the screen, 247 the bottom  ( 247 -24  = 223  range )
 // Shooting gallary  ~32 is the top of the playfield, ~230 is the bottom ( 230 -32 = 198 range. but would need offset due to border)
 
 
 // My SGDK test with SMS STUNNER appears to return 30 through 180 when I pan
 // across my TV screen in H32 mode.   so about 150 values over 320 pixels
-byte x = 100; // 15 appears to give me about 30, 193 gets me to about 180  ( 193-15 = 178 range )
+volatile byte x = 100; // 15 appears to give me about 30, 193 gets me to about 180  ( 193-15 = 178 range )
 // Shooting gallary  ~22 is the left of the playfield, ~170 is the bottom ( 187 -22 = 165 range. but would need offset due to border)
 
 
@@ -82,11 +82,11 @@ void setup() {
 
   // controller pins 
   pinMode(STUNNER_PIN_4, OUTPUT);
-  pinMode(STUNNER_PIN_5, OUTPUT);
-  pinMode(STUNNER_PIN_6, OUTPUT); 
+  pinMode(STUNNER_PIN_5_START, OUTPUT);
+  pinMode(STUNNER_PIN_6_TRIGGER, OUTPUT); 
   digitalWrite(STUNNER_PIN_4, HIGH);
-  digitalWrite(STUNNER_PIN_5, HIGH);
-  digitalWrite(STUNNER_PIN_6, HIGH);
+  digitalWrite(STUNNER_PIN_5_START, HIGH);
+  digitalWrite(STUNNER_PIN_6_TRIGGER, HIGH);
 
   // interrupts
   attachInterrupt(digitalPinToInterrupt(COMPOSITE_SYNC_1881), compositeSyncInterrrupt, RISING);
@@ -95,11 +95,43 @@ void setup() {
 
 void loop() { 
   Serial.println(verticalLine);
-  // pull the trigger every once in awhile
-  delay(500);
-  digitalWrite(STUNNER_PIN_6, HIGH); 
-  delay(500);
-  digitalWrite(STUNNER_PIN_6, LOW); 
-  delay(500);
-  digitalWrite(STUNNER_PIN_6, LOW); 
+  
+  if (Serial.available()){
+    char val = Serial.read(); // Read a character
+    switch(val) {
+      case 'w':
+        y++;
+        break;
+      case 'a':
+        x--;
+        break;
+      case 's':
+        y--;
+        break;
+      case 'd':
+        x++;
+        break;
+      case 'W':
+        y +=10;
+        break;
+      case 'A':
+        x -= 10;
+        break;
+      case 'S':
+        y -= 10;
+        break;
+      case 't':  
+        digitalWrite(STUNNER_PIN_6_TRIGGER, LOW); 
+        delay(300);
+        digitalWrite(STUNNER_PIN_6_TRIGGER, HIGH);     
+        break; 
+      case 'r':
+        digitalWrite(STUNNER_PIN_5_START, LOW); 
+        delay(300);
+        digitalWrite(STUNNER_PIN_5_START, HIGH);  
+        break; 
+    }
+
+ 
+  }
 }
