6eccf1318d64eb602b7d98726002e6ee5f156186
Greg Gallardo <greggallardo@gmail.com>
2022-10-02 23:53:13 -0500

    working on new menacer implementation

--- arduino/menacer/menacer.ino
+++ arduino/menacer/menacer.ino
@@ -20,7 +20,7 @@
 // Leonardo Bluetooth use real TX/TR
 #define SERIAL_COM Serial1
 #else
-// Use USB 
+// Use USB
 #define SERIAL_COM Serial
 #endif
 
@@ -31,23 +31,25 @@ const int VERTICAL_SYNC_1881 = 3;
 volatile int verticalLine = 0;
 
 // controller pins
-const int TH_PIN = 8;
+const int TL_PIN = 7;
+const int TH_PIN = 8;    // PB0
 
-const int B_PIN = 9;
-const int A_PIN = 10;
-const int C_PIN = 11;
-const int S_PIN = 12;
+const int B_PIN = 9;     // PB1
+const int A_PIN = 10;    // PB2
+const int C_PIN = 11;    // PB3
+const int S_PIN = 12;    // PB7
+
 
 // offsets
 const int minY = 30;
 const int minX = 70;
 
 // input variables
-byte y = 140;  // line count
-byte x = 200;  // Simulate TH delay
-byte buttons = 0;
+volatile byte y = 140;  // line count for aim
+volatile byte x = 200;  // Simulate TH delay for aim
+volatile byte buttons = 0;
 
-// modified delayMicroseconds to use the smallest possible wait
+// I modified delayMicroseconds to use the smallest possible wait
 // to increase horizontal resolution
 void delayX4Cycles(unsigned int c)
 {
@@ -68,13 +70,47 @@ void compositeSyncInterrrupt() {
     digitalWrite( TH_PIN, HIGH );
     delayMicroseconds( 5 ); // arbitrary.
     digitalWrite( TH_PIN, LOW );
-
   }
 }
 
 void verticalSyncInterrrupt() {
   verticalLine = 0;
 }
+
+void TLInterrupt() {
+  int tlState = digitalRead(TL_PIN);
+
+  if ( tlState == LOW ) {
+    // faling edge, so deactivate all of the buttons, but leave TH ( PB0 ) alone
+    // PB1 - B
+    // PB2 - A/Trigger
+    // PB3 - C
+    // PB7 - Start
+    PORTB = PORTB & B01110001;  // bit-zero is TH,  4,5,6 are not used
+  } else {
+    // rising edge, set the buttons as needed.
+    byte pb = PORTB;
+    // TODO: May make more sense to set PB* bits in Raspberry PI properly.
+    if ( buttons & 0x01 ) {
+      // B
+      pb = pb | 2;
+    }
+    if ( buttons & 0x02 ) {
+      // A - trigger
+      pb = pb | 4;
+    }
+    if ( buttons & 0x04 ) {
+      // C
+      pb = pb | 8;
+    }
+    if ( buttons & 0x08) {
+      // S
+      pb = pb | 64;
+    }
+    PORTB = pb;
+  }
+}
+
 
 void setup() {
   // serial communication
@@ -83,23 +119,26 @@ void setup() {
   // Sync Splitter
   pinMode(COMPOSITE_SYNC_1881, INPUT);
   pinMode(VERTICAL_SYNC_1881, INPUT);
+  // TL line from Sega Genesis / MegaDrive
+  pinMode(TL_PIN, INPUT);
 
   // controller pins
   pinMode(TH_PIN, OUTPUT);
   digitalWrite(TH_PIN, LOW);
 
   pinMode(B_PIN, OUTPUT);
-  digitalWrite(B_PIN, HIGH);
+  digitalWrite(B_PIN, LOW);
   pinMode(A_PIN, OUTPUT);
-  digitalWrite(A_PIN, HIGH);
+  digitalWrite(A_PIN, LOW);
   pinMode(C_PIN, OUTPUT);
-  digitalWrite(C_PIN, HIGH);
+  digitalWrite(C_PIN, LOW);
   pinMode(S_PIN, OUTPUT);
-  digitalWrite(S_PIN, HIGH);
+  digitalWrite(S_PIN, LOW);
 
-  // interrupts
+  // Setup Interrupts
   attachInterrupt(digitalPinToInterrupt(COMPOSITE_SYNC_1881), compositeSyncInterrrupt, RISING);
   attachInterrupt(digitalPinToInterrupt(VERTICAL_SYNC_1881), verticalSyncInterrrupt, RISING);
+  attachInterrupt(digitalPinToInterrupt(TL_PIN), TLInterrupt, CHANGE); // PIN 7 interrupt requires leonardo (or equiv)
 }
 
 void loop() {
@@ -107,30 +146,7 @@ void loop() {
   {
     x = SERIAL_COM.read();
     y = SERIAL_COM.read();
-    buttons = SERIAL_COM.read(); 
-      // set buttons
-    if ( buttons & 0x01 ) {
-      digitalWrite(B_PIN, LOW);
-    } else {
-      digitalWrite(B_PIN, HIGH);
-    }
-    if ( buttons & 0x02 ) {
-      digitalWrite(A_PIN, LOW);
-    } else {
-      digitalWrite(A_PIN, HIGH);
-    }
-
-    if ( buttons & 0x04 ) {
-      digitalWrite(C_PIN, LOW);
-    } else {
-      digitalWrite(C_PIN, HIGH);
-    }
-
-    if ( buttons & 0x08 ) {
-      digitalWrite(S_PIN, LOW);
-    } else {
-      digitalWrite(S_PIN, HIGH);
-    }
+    buttons = SERIAL_COM.read();
   }
 
 }
